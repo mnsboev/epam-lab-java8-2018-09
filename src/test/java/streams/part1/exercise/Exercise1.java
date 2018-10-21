c40ae5d66fb2de8d641364e0c9e83939cf98fa4a
mnsboev <m.n.sboev@gmail.com>
2018-10-21 11:25:47 +0300

    homework lambda.streams.part1.exercise1 done

--- src/test/java/streams/part1/exercise/Exercise1.java
+++ src/test/java/streams/part1/exercise/Exercise1.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
@@ -22,8 +23,12 @@ class Exercise1 {
     void findPersonsEverWorkedInEpam() {
         List<Employee> employees = getEmployees();
 
-        // TODO реализация, использовать Collectors.toList()
-        List<Person> personsEverWorkedInEpam = null;
+        List<Person> personsEverWorkedInEpam = employees.stream()
+                .filter(employee -> employee.getJobHistory().
+                        stream().
+                        anyMatch(entry -> entry.getEmployer().equals("EPAM")))
+                .map(Employee::getPerson)
+                .collect(Collectors.toList());
 
         assertThat(personsEverWorkedInEpam, contains(
                 employees.get(0).getPerson(),
@@ -37,8 +42,13 @@ class Exercise1 {
     void findPersonsBeganCareerInEpam() {
         List<Employee> employees = getEmployees();
 
-        // TODO реализация, использовать Collectors.toList()
-        List<Person> startedFromEpam = null;
+        List<Person> startedFromEpam = employees.stream()
+                .filter(employee -> employee.getJobHistory()
+                        .stream()
+                        .limit(1)
+                        .anyMatch(entry -> entry.getEmployer().equals("EPAM")))
+                .map(Employee::getPerson)
+                .collect(Collectors.toList());
 
         assertThat(startedFromEpam, contains(
                 employees.get(0).getPerson(),
@@ -51,8 +61,10 @@ class Exercise1 {
     void findAllCompanies() {
         List<Employee> employees = getEmployees();
 
-        // TODO реализация, использовать Collectors.toSet()
-        Set<String> companies = null;
+        Set<String> companies = employees.stream()
+                .flatMap(employee -> employee.getJobHistory().stream())
+                .map(JobHistoryEntry::getEmployer)
+                .collect(Collectors.toSet());
 
         assertThat(companies, containsInAnyOrder("EPAM", "google", "yandex", "mail.ru", "T-Systems"));
     }
@@ -61,8 +73,10 @@ class Exercise1 {
     void findMinimalAgeOfEmployees() {
         List<Employee> employees = getEmployees();
 
-        // TODO реализация
-        Integer minimalAge = null;
+        Integer minimalAge = employees.stream()
+                .map(Employee::getPerson)
+                .mapToInt(Person::getAge)
+                .min().getAsInt();
 
         assertThat(minimalAge, is(21));
     }
