56b9af8145abbafa538f01059459a841ce75927d
mikhail <mikhail_sboev@epam.com>
2018-10-30 13:05:30 +0300

    Homework.streams.part2.exercise3 done

--- src/test/java/streams/part2/exercise/Exercise3.java
+++ src/test/java/streams/part2/exercise/Exercise3.java
@@ -2,6 +2,10 @@ package streams.part2.exercise;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.joining;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -12,7 +16,11 @@ class Exercise3 {
     void createLimitedStringWithOddNumbersSeparatedBySpaces() {
         int countNumbers = 10;
 
-        String result = null;
+        String result = IntStream.iterate(0, x -> x + 1)
+                .filter(x -> x % 2 != 0)
+                .limit(countNumbers)
+                .mapToObj(String::valueOf)
+                .collect(joining(" "));
 
         assertThat(result, is("1 3 5 7 9 11 13 15 17 19"));
     }
@@ -20,8 +28,10 @@ class Exercise3 {
     @Test
     void extractEvenNumberedCharactersToNewString() {
         String source = "abcdefghijklm";
-
-        String result = null;
+        String result = IntStream.range(0, source.length())
+                .filter(x -> x % 2 == 0)
+                .mapToObj(x -> String.valueOf(source.charAt(x)))
+                .collect(Collectors.joining(""));
 
         assertThat(result, is("acegikm"));
     }
